5bba6dd42ec7242d086b8530b8ab3d84024d99dc
Admin <phamminh201207@gmail.com>
2026-03-27 15:26:04 +0700

    feat: tạo class cơ sở Entity với equals và hashCode

--- shared/src/main/java/com/auction/shared/model/entity/Entity.java
+++ shared/src/main/java/com/auction/shared/model/entity/Entity.java
@@ -1,2 +1,41 @@
-package com.auction.shared.model.entity;public class Entity {
+package com.auction.shared.model.entity;
+
+import java.io.Serializable;
+
+public abstract class Entity implements Serializable {
+    // serialVersionUID giúp đảm bảo tính đồng bộ khi mã hóa/giải mã đối tượng giữa Client và Server
+    private static final long serialVersionUID = 1L;
+    // Encapsulation: Thuộc tính được bảo vệ, chỉ các lớp con (Item, User) mới truy cập trực tiếp được
+    protected int id;
+
+    // Constructor mặc định
+    public Entity() {}
+
+    // Constructor khởi tạo với ID
+    public Entity(int id) {
+        this.id = id;
+    }
+
+    // Getter và Setter cho ID
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Entity entity = (Entity) o;
+        return id == entity.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(id);
+    }
 }
