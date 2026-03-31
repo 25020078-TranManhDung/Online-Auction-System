ffae6782fa04943a7f26456ec8a5220fae2b6285
Admin <phamminh201207@gmail.com>
2026-03-31 15:43:04 +0700

    feat : tạo enum UserRole

---
+++ shared/src/main/java/com/auction/shared/enums/UserRole.java
@@ -0,0 +1,8 @@
+package com.auction.shared.enums;
+
+//Định nghĩa các vai trò của người dùng trong hệ thống đấu giá.
+public enum UserRole {
+    BIDDER, // Người tham gia đấu giá
+    SELLER, // Người đăng bán sản phẩm
+    ADMIN   // Người quản trị hệ thống
+}
