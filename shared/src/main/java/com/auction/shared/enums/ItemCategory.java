f065ebca0d6c887d4fedb9e2607e09474f42d084
Admin <phamminh201207@gmail.com>
2026-03-31 15:53:19 +0700

    feat : tạo enum ItemCategory

---
+++ shared/src/main/java/com/auction/shared/enums/ItemCategory.java
@@ -0,0 +1,8 @@
+package com.auction.shared.enums;
+// Định nghĩa danh mục của các sản phẩm đấu giá.
+public enum ItemCategory {
+    ELECTRONICS, // Đồ điện tử
+    ART,         // Nghệ thuật
+    VEHICLE,     // Phương tiện
+    OTHER        // Các loại sản phẩm khác
+}
